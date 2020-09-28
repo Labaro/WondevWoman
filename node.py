ef7e4ccb27f8e7c89ee45ee5313fa1b34ba20644
Labaro <e.levecque@hotmail.fr>
2020-09-28 18:43:03 +0200

    feat(node): tree structure with negamax and alpha-beta pruning

---
+++ node.py
@@ -0,0 +1,52 @@
+class Node(object):
+    """Node object is a box to handle tree exploration. All information about the game are store in the state object."""
+
+    def __init__(self, state, parent=None):
+        if parent is None:
+            parent = []
+        self.state = state
+        self.children = []
+        self.parent = parent
+        self.value = 0
+
+    def __hash__(self):
+        return self.state.__hash__()
+
+    def __eq__(self, other):
+        return self.state.__eq__(other.state)
+
+    def get_children(self):
+        for action in self.state.get_actions():
+            self.children.append(Node(self.state.simulate(action), [self]))
+        return self.children
+
+    def evaluate(self):
+        return self.state.evaluate()
+
+    def is_terminal(self):
+        return self.state.is_terminal()
+
+    def negamax(self, depth):
+        if depth == 0 or self.is_terminal():
+            self.value = self.evaluate()
+            return self.value
+        children = self.get_children()
+        value = -1e17
+        for child in children:
+            value = max(value, -child.negamax(depth - 1))
+        self.value = value
+        return self.value
+
+    def negamax_alpha_beta_pruning(self, depth, alpha, beta):
+        if depth == 0 or self.is_terminal():
+            self.value = self.evaluate()
+            return self.evaluate()
+        children = self.get_children()
+        value = -1e17
+        for child in children:
+            value = max(value, -child.negamax_alpha_beta_pruning(depth - 1, -beta, -alpha))
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        self.value = value
+        return self.value
---
+++ test.py
@@ -0,0 +1,65 @@
+import unittest
+
+from node import Node
+
+
+class TestState(object):
+    def __init__(self, value, final, actions):
+        self.value = value
+        self.final = final
+        self.actions = actions
+        self.eval = False
+
+    def __repr__(self):
+        return str(self.value)
+
+    def is_terminal(self):
+        return self.final
+
+    def evaluate(self):
+        self.eval = True
+        return self.value
+
+    def get_actions(self):
+        return self.actions
+
+    def simulate(self, action):
+        return action
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.state1 = TestState(2, True, [])
+        self.state2 = TestState(3, True, [])
+        self.state3 = TestState(0, True, [])
+        self.state4 = TestState(1, True, [])
+        self.state5 = TestState(3, True, [])
+        self.state6 = TestState(-3, True, [])
+        self.state7 = TestState(-1, True, [])
+        self.state8 = TestState(3, False, [self.state6, self.state7])
+        self.state9 = TestState(-2, False, [self.state1, self.state2])
+        self.state10 = TestState(0, False, [self.state3, self.state4])
+        self.state11 = TestState(-3, False, [self.state5, self.state8])
+        self.state12 = TestState(3, False, [self.state9, self.state10, self.state11])
+
+        self.root = Node(self.state12)
+
+    def test_alpha_pruning(self):
+        self.root.negamax_alpha_beta_pruning(3, -1e17, 1e17)
+        assert not self.state4.eval
+
+    def test_beta_pruning(self):
+        self.root.negamax_alpha_beta_pruning(3, -1e17, 1e17)
+        assert not self.state7.eval
+
+    def test_negamax_with_alpha_beta_pruning(self):
+        self.root.negamax_alpha_beta_pruning(3, -1e17, 1e17)
+        assert self.root.value == self.root.state.value
+
+    def test_negamax(self):
+        self.root.negamax(3)
+        assert self.root.value == self.root.state.value
+
+
+if __name__ == '__main__':
+    unittest.main()
