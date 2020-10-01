a2c9fa47318fd7ce21598e3d54db94be53800f87
Samy <samy.zouhri.etu@univ-lille.fr>
2020-10-01 02:46:25 +0200

    Testing the method get_accessible_cells

--- diagram_class.py
+++ diagram_class.py
@@ -1,14 +1,32 @@
 from scipy.spatial import distance
+from numpy import array
 
 class Cell():
     
-    def __init__(self, pos, height):
+    def __init__(self, pos=(0,0), height=0):
         self.pos = pos
         self.height = height
+        
+    def __iter__(self):
+        yield (self.pos)
         
     def is_valid_cell(self):
         if self.height != 4:
             return True
+        else:
+            return('Dead')
+        
+    def get_accessible_cells(self):
+        """
+        Determine ,for a fix cell, all the accessible cells around it 
+        """
+        accessible_dir = []
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if (i != 0 or j != 0):
+                    if Cell(tuple(array(tuple(self.pos)+ array((i,j))))).is_valid_cell() == True:
+                        accessible_dir.append(tuple(array(tuple(self.pos)+ array((i,j)))))
+        return accessible_dir
     
 class Position(Cell):
     def __init__(self, pos, height):
@@ -19,18 +37,8 @@ class Position(Cell):
         return distance.chebyshev(self.pos,pos2)
     
 class Units():
-    def __init__(self, player, pos):
+    def __init__(self, pos, player, index):
         self.player = player
-        self.pos = pos 
-        
-    def get_accessible_cells(self):
-        """
-        Determine ,for an unit, all the accessible cells around it 
-        """
-        accessible_pos = []
-        for i in range(-1,1):
-            for j in range(-1,1):
-                if i != 0 and j != 0:
-                    if Cell(self.pos + (i,j)).is_valid_cell() == True:
-                        accessible_pos.append(self.pos + (i,j))
-        return accessible_pos
+        self.pos = pos
+        self.index = index
+               
--- diagram_class_test.py
+++ diagram_class_test.py
@@ -1,6 +1,6 @@
 import unittest
-from diagram_class import Cell, Position
-import numpy as np
+from diagram_class import Cell, Position, Units
+
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,6 +12,18 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(Position((2,2),3).distance_to((2,3)),1)
         self.assertEqual(Position((2,2),3).distance_to((2,4)),2)
         self.assertEqual(Position((2,2),3).distance_to((1,1)),1)
+        
+    def test_accessible_cells(self):
+        cell_center = Cell((1,1),3)
+        cell1 = Cell((0,0), 2)
+        cell2 = Cell((0,1), 3)
+        cell3 = Cell((0,2), 4)
+        cell4 = Cell((1,0),4)
+        cell5 = Cell((1,2),1)
+        cell6 = Cell((2,0),4)
+        cell7 = Cell((2,1),0)
+        cell8 = Cell((2,2),4)
+        self.assertEqual(cell_center.get_accessible_cells(),[(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)])
         
 
 if __name__ == '__main__':
