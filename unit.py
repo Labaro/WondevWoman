4c74de8ddf5066d2f024d5990b81a38c304e2873
Labaro <e.levecque@hotmail.fr>
2020-10-06 23:03:41 +0200

    fix(unit): adding index attribute

--- unit.py
+++ unit.py
@@ -2,10 +2,11 @@ from utils import *
 
 
 class Unit(object):
-    def __init__(self, x, y, player):
+    def __init__(self, x, y, player, index):
         self.x = x
         self.y = y
         self.player = player
+        self.index = index
 
     def get_accessible_positions(self, grid):
         for x, y in get_valid_neighbours(grid, self.x, self.y):
