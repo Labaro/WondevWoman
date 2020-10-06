64abdbbd276ef0ee15534e316cbbf09b5161d86d
Labaro <e.levecque@hotmail.fr>
2020-10-06 18:29:32 +0200

    feat(action): Adding class action

---
+++ action.py
@@ -0,0 +1,21 @@
+class Action(object):
+    def __init__(self, index, dir_1, dir_2):
+        self.index = index
+        self.dir_1 = dir_1
+        self.dir_2 = dir_2
+
+class MoveAction(Action):
+    def __init__(self, index, dir_1, dir_2):
+        super().__init__(index, dir_1, dir_2)
+
+    def __str__(self):
+        return f"MOVE&BUILD {self.index} {self.dir_1} {self.dir_2}"
+
+
+class PushAction(Action):
+    def __init__(self, index, dir_1, dir_2):
+        super().__init__(index, dir_1, dir_2)
+
+    def __str__(self):
+        return f"PUSH&BUILD {self.index} {self.dir_1} {self.dir_2}"
+
