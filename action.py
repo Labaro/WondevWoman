a5db69c8adb9b4adcbfb54ab435a306430dc28ae
Labaro <e.levecque@hotmail.fr>
2020-10-06 23:34:14 +0200

    feat: adding repr method

--- action.py
+++ action.py
@@ -11,11 +11,17 @@ class MoveAction(Action):
     def __str__(self):
         return f"MOVE&BUILD {self.index} {self.dir_1} {self.dir_2}"
 
+    def __repr__(self):
+        return f"MOVE&BUILD {self.index} {self.dir_1} {self.dir_2}"
+
 
 class PushAction(Action):
     def __init__(self, index, dir_1, dir_2):
         super().__init__(index, dir_1, dir_2)
 
     def __str__(self):
+        return f"PUSH&BUILD {self.index} {self.dir_1} {self.dir_2}"
+
+    def __repr__(self):
         return f"PUSH&BUILD {self.index} {self.dir_1} {self.dir_2}"
 
