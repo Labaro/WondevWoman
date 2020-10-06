1face2ff515da99d964cd394f0e8c78eadfd1cb8
Labaro <e.levecque@hotmail.fr>
2020-10-06 18:48:23 +0200

    feat(unit): adding get_accessible_positions method

--- unit.py
+++ unit.py
@@ -1,6 +1,13 @@
+from utils import *
+
+
 class Unit(object):
     def __init__(self, x, y, player):
         self.x = x
         self.y = y
         self.player = player
-        
+
+    def get_accessible_positions(self, grid):
+        for x, y in get_valid_neighbours(grid, self.x, self.y):
+            if grid[y][x] - grid[self.y][self.x] <= 1:
+                yield x, y
