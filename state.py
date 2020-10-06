3b2442129f6d056f77122b98ce220dbe921bc312
Labaro <e.levecque@hotmail.fr>
2020-10-07 00:19:22 +0200

    feat(state): adding evaluate and is_terminal methods

--- state.py
+++ state.py
@@ -10,15 +10,28 @@ class State(object):
         self.player = player
         self.turn = turn
         self.legal_actions = []
+        self.score = 0
+        self.has_been_evaluated = False
 
     def evaluate(self):
-        pass
+        if self.has_been_evaluated:
+            return self.score
+        else:
+            if self.is_terminal():
+                self.score -= 1000
+            for unit in self.units:
+                if unit.player == self.player:
+                    self.score += self.grid[unit.y][unit.x] * 5
+                else:
+                    self.score -= self.grid[unit.y][unit.x] * 2
+            self.has_been_evaluated = True
+            return self.score
 
     def get_legal_actions(self):
         if self.legal_actions:
             return self.legal_actions
         for unit in self.units:
-            others_positions = [(u.x, u.y) for u in self.units if u!= unit]
+            others_positions = [(u.x, u.y) for u in self.units if u != unit]
             if unit.player == self.player:
                 for move_x, move_y in unit.get_accessible_positions(self.grid):
                     for build_x, build_y in get_valid_neighbours(self.grid, move_x, move_y):
@@ -30,7 +43,7 @@ class State(object):
                     dir_1 = position_to_direction(unit.x, unit.y, push_x, push_y)
                     idx = DIRECTIONS.index(dir_1)
                     for i in range(-1, 2):
-                        dir_2 = DIRECTIONS[(idx + i ) % 8]
+                        dir_2 = DIRECTIONS[(idx + i) % 8]
                         push_to_x, push_to_y = direction_to_position(push_x, push_y, dir_2)
                         if (push_x >= 0 and push_x < len(self.grid) and push_y >= 0 and push_y < len(self.grid) and
                                 self.grid[push_to_y][push_to_x] - self.grid[push_y][push_x] <= 1):
@@ -38,7 +51,7 @@ class State(object):
         return self.legal_actions
 
     def is_terminal(self):
-        pass
+        return bool(self.get_legal_actions())
 
     def simulate(self, action):
         """We suppose that every actions that can be simulate are already legal actions"""
@@ -65,4 +78,3 @@ class State(object):
                         self.grid[build_y][build_x] = -1
                     break
         return State(self.grid[:][:], self.units, - self.player, self.turn + 1)
-
