92b1bafbdf9293e071721110c29cff2001e5919d
Labaro <e.levecque@hotmail.fr>
2020-10-07 10:35:53 +0200

    feat(utils): adding a basic track_units method

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+import random as rd
+
+from unit import Unit
+
 DIRECTIONS = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+
 
 def get_valid_neighbours(grid, x, y):
     size = len(grid)
@@ -33,3 +38,16 @@ def position_to_direction(from_x, from_y, to_x, to_y):
     elif diff_x == -1:
         dir += 'W'
     return dir
+
+
+def track_units(player, player_units, known_units, grid):
+    """Not visible units are simply randomly placed on the map."""
+    visible_position = []
+    for unit in player_units:
+        visible_position.append((unit.x, unit.y))
+        visible_position += [(x, y) for x, y in get_valid_neighbours(grid, unit.x, unit.y)]
+    valid_positions = [(x, y) for x in range(len(grid)) for y in range(len(grid)) if grid[y][x] >= 0]
+    not_visible_positions = set(valid_positions) - set(visible_position)
+    while len(known_units) < 2:
+        known_units.append(Unit(*rd.choice(not_visible_positions), len(known_units), - player))
+    return known_units
